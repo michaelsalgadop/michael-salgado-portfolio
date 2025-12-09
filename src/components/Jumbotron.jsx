1cede51e577d0793ef64e1d868a643f29545f8d9
Michaelsp11 <michaelsalgadop11@gmail.com>
2025-12-09 19:17:55 +0100

    En pantallas grandes, no se veía correctamente la web, código estaba muy separado había mucho espacio por medio

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -2,14 +2,11 @@ import { ImageJumbotron } from "./Jumbotron/ImageJumbotron";
 import { TitlePage } from "./Jumbotron/TitlePage";
 export const Jumbotron = () => {
   return (
-    <section
-      className="relative grid grid-cols-12 gap-4 min-h-150"
-      id="home"
-    >
+    <section className="relative grid grid-cols-12 gap-4 min-h-150" id="home">
       <div className="flex items-center col-span-12 lg:col-span-7 mb-5">
         <TitlePage />
       </div>
-      <div className="flex justify-center items-center col-span-12 lg:col-span-5">
+      <div className="flex justify-center lg:justify-end items-center col-span-12 lg:col-span-5">
         <ImageJumbotron />
       </div>
     </section>
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -12,86 +12,89 @@ export default function Layout() {
   return (
     <div className="bg-gray-900">
       {openNavbar && <DarkScreen />}
-      <Header />
-      <main className="text-white space-y-10 lg:space-y-15">
-        <Jumbotron />
-        <About />
-        <Experiences />
-        <section>
-          <a href="https://rentari.netlify.app/" target="_blank">
-            <h3>PROJECTOS</h3>
+      <div className="mx-auto min-h-screen max-w-7xl">
+        <Header />
+        <main className="text-white space-y-10 lg:space-y-15">
+          <Jumbotron />
+          <About />
+          <Experiences />
+          <section>
+            <a href="https://rentari.netlify.app/" target="_blank">
+              <h3>PROJECTOS</h3>
+              <p>
+                Rentari <i></i>
+              </p>
+              <p>
+                Web de renting de vehículos pensada tanto para empresas como
+                para particulares, diseñada con el objetivo de ofrecer una
+                experiencia moderna, segura y fácil de utilizar.
+              </p>
+              <img src={rentariImg} alt="Imagen de la web de Rentari" />
+            </a>
+          </section>
+          <Educations />
+          <section>
+            <h3>CERTIFICACIONES</h3>
+            <p>2021</p>
             <p>
-              Rentari <i></i>
+              CODING BOOTCAMP DESARROLLADOR WEB FULL STACK JAVASCRIPT (258
+              HORAS)
             </p>
-            <p>
-              Web de renting de vehículos pensada tanto para empresas como para
-              particulares, diseñada con el objetivo de ofrecer una experiencia
-              moderna, segura y fácil de utilizar.
-            </p>
-            <img src={rentariImg} alt="Imagen de la web de Rentari" />
-          </a>
-        </section>
-        <Educations />
-        <section>
-          <h3>CERTIFICACIONES</h3>
-          <p>2021</p>
+            <ul>
+              <li>Las TIC.</li>
+              <li>Tecnologías y diseño Web, HTML i CSS.</li>
+              <li>Algoritmos y programas. Programación Backend.</li>
+              <li>Almacenes de datos.</li>
+              <li>Interacción con el usuario. FrontEnd con ReactJS.</li>
+              <li>Proyecto. Integración FrontEnd/BackEnd.</li>
+            </ul>
+          </section>
+          <section>
+            <h3>Idiomas</h3>
+            <div className="progress">
+              <div className="progress-bar">
+                <span className="progress-bar-text">Nativo</span>
+              </div>
+            </div>
+            <span>Castellano</span>
+            <div>
+              <div className="progress-bar">
+                <span className="progress-bar-text">Nativo</span>
+              </div>
+            </div>
+            <span>Catalán</span>
+            <div>
+              <div className="progress-bar">
+                <span className="progress-bar-text">Profesional</span>
+              </div>
+            </div>
+            <span>Inglés</span>
+          </section>
+          <section>
+            <h3>OTROS DATOS</h3>
+            <ul>
+              <li>
+                <i></i>Incorporación immediata
+              </li>
+              <li>
+                <i></i>Disponibilidad horária completa
+              </li>
+              <li>
+                <i></i>Permiso de conducir B1
+              </li>
+            </ul>
+          </section>
+        </main>
+        <footer>
+          <p>Trabajemos juntos! El futuro es hoy...</p>
           <p>
-            CODING BOOTCAMP DESARROLLADOR WEB FULL STACK JAVASCRIPT (258 HORAS)
+            Diseñado libremente con <a href="https://es.react.dev/">React</a> y
+            <a href="https://tailwindcss.com/">Tailwind CSS</a> y programado en
+            <a href="https://code.visualstudio.com/">Visual Studio Code</a> por
+            mí. Implementado con <a href="https://www.netlify.com/">Netlify</a>.
           </p>
-          <ul>
-            <li>Las TIC.</li>
-            <li>Tecnologías y diseño Web, HTML i CSS.</li>
-            <li>Algoritmos y programas. Programación Backend.</li>
-            <li>Almacenes de datos.</li>
-            <li>Interacción con el usuario. FrontEnd con ReactJS.</li>
-            <li>Proyecto. Integración FrontEnd/BackEnd.</li>
-          </ul>
-        </section>
-        <section>
-          <h3>Idiomas</h3>
-          <div className="progress">
-            <div className="progress-bar">
-              <span className="progress-bar-text">Nativo</span>
-            </div>
-          </div>
-          <span>Castellano</span>
-          <div>
-            <div className="progress-bar">
-              <span className="progress-bar-text">Nativo</span>
-            </div>
-          </div>
-          <span>Catalán</span>
-          <div>
-            <div className="progress-bar">
-              <span className="progress-bar-text">Profesional</span>
-            </div>
-          </div>
-          <span>Inglés</span>
-        </section>
-        <section>
-          <h3>OTROS DATOS</h3>
-          <ul>
-            <li>
-              <i></i>Incorporación immediata
-            </li>
-            <li>
-              <i></i>Disponibilidad horária completa
-            </li>
-            <li>
-              <i></i>Permiso de conducir B1
-            </li>
-          </ul>
-        </section>
-      </main>
-      <footer>
-        <p>Trabajemos juntos! El futuro es hoy...</p>
-        <p>
-          Diseñado libremente con <a href="https://es.react.dev/">React</a> y
-          <a href="https://tailwindcss.com/">Tailwind CSS</a> y programado en
-          <a href="https://code.visualstudio.com/">Visual Studio Code</a> por
-          mí. Implementado con <a href="https://www.netlify.com/">Netlify</a>.
-        </p>
-      </footer>
+        </footer>
+      </div>
     </div>
   );
 }
