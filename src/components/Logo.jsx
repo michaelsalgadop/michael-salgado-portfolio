0b424f1b8399819347e6ed861f586104a8ea817b
Michaelsp11 <michaelsalgadop11@gmail.com>
2025-12-15 19:39:09 +0100

    Cambio de CV, estilización del footer y icono de linkedin

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import logo from "./logo.png";
+export const Logo = () => {
+  return (
+    <img
+      src={logo}
+      alt="Logo"
+      title="logo"
+      className="size-full object-cover"
+    />
+  );
+};
