0d7f7adfbee1370d1d13ae1551e551f7ab26182e
Michaelsp11 <michaelsalgadop11@gmail.com>
2025-12-29 18:26:20 +0100

    Proyectos componetizados en mini componentes

---
+++ src/components/Project/ProjectOverlay.jsx
@@ -0,0 +1,5 @@
+export const ProjectOverlay = () => {
+  return (
+    <div className="bg-black opacity-100 lg:opacity-0 lg:group-hover:opacity-100 absolute top-0 left-0 size-full transition-all duration-450 ease-in-out"></div>
+  );
+};
