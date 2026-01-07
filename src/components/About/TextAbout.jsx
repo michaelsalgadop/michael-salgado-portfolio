12488ae114dce21020e2d936029d66ae6ee1e2b7
Michaelsp11 <michaelsalgadop11@gmail.com>
2026-01-07 18:03:53 +0100

    Feat: componente TitleSection para los titulos de cada seccion y evitar redundancia

--- src/components/About/TextAbout.jsx
+++ src/components/About/TextAbout.jsx
@@ -1,37 +1,40 @@
+import { TitleSection } from "../UI/TitleSection";
 import { AboutItem } from "./AboutItem";
 import { Introduction } from "./Introduction";
 
 export const TextAbout = () => {
   return (
     <>
-      <h3 className="text-center lg:text-left text-[35px] font-bold tracking-tight mb-2">
-        Sobre<span className="text-tertiary ml-2">Mi</span>
-      </h3>
+      <TitleSection
+        classTitle="title-section-about"
+        titleWithoutHighlighting="Sobre"
+        titleHighlighting="Mi"
+      />
       <Introduction />
       <ul className="space-y-4 mt-5">
         <AboutItem
           title="Resolutivo"
           text="Aporto nuevas ideas y soluciones a cualquier circunstancia y me
             enfoco en las necesidades del cliente."
-          classTitle="title-about after:w-31"
+          classTitle="title-item-about after:w-31"
           classText="text-about"
         />
         <AboutItem
           title="Responsable"
           text="Concienciado en cuidar cada detalle, cada pixel importa."
-          classTitle="title-about after:w-38"
+          classTitle="title-item-about after:w-38"
           classText="text-about"
         />
         <AboutItem
           title="Constante"
           text="Cada objetivo debe cumplirse en la fecha o evento acordado."
-          classTitle="title-about after:w-31"
+          classTitle="title-item-about after:w-31"
           classText="text-about"
         />
         <AboutItem
           title="Colaborativo"
           text="Una barca no se mueve por un solo navegante. Rememos juntos en una misma dirección."
-          classTitle="title-about after:w-38"
+          classTitle="title-item-about after:w-38"
           classText="text-about"
         />
       </ul>
--- src/components/Education/Certifications.jsx
+++ src/components/Education/Certifications.jsx
@@ -1,15 +1,17 @@
 import { useTranslation } from "react-i18next";
 import { certifications } from "../../data/certifications";
 import { Card } from "../Card";
+import { TitleSection } from "../UI/TitleSection";
 
 export const Certifications = () => {
   const { t } = useTranslation();
   return (
     <>
-      <h3 className="title-section">
-        {t("plural-mine")}
-        <span className="text-tertiary ml-2">{t("certification")}</span>
-      </h3>
+      <TitleSection
+        classTitle="title-section"
+        titleWithoutHighlighting={t("plural-mine")}
+        titleHighlighting={t("certification")}
+      />
       <div className="flex justify-center gap-4">
         {certifications &&
           certifications.map(
